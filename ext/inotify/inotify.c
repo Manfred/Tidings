503a4d0db647d8b48dcd4290742febcc8233a11e
Manfred Stienstra <manfred@fngtps.com>
2013-11-19 22:40:49 +0100

    Call processor with flags from Inotify.

--- ext/inotify/inotify.c
+++ ext/inotify/inotify.c
@@ -9,6 +9,26 @@
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
 
+VALUE inotify_event_flags(uint32_t mask)
+{
+  VALUE event_flags = rb_ary_new();
+
+  if (mask & IN_ACCESS)
+    rb_ary_push(event_flags, ID2SYM(rb_intern("accessed")));
+  if (mask & IN_CREATE)
+    rb_ary_push(event_flags, ID2SYM(rb_intern("created")));
+  if (mask & IN_DELETE)
+    rb_ary_push(event_flags, ID2SYM(rb_intern("removed")));
+  if (mask & IN_MODIFY)
+    rb_ary_push(event_flags, ID2SYM(rb_intern("modified")));
+  if (mask & IN_ISDIR)
+    rb_ary_push(event_flags, ID2SYM(rb_intern("dir")));
+  else
+    rb_ary_push(event_flags, ID2SYM(rb_intern("file")));
+
+  return event_flags;
+}
+
 /*
  *  call-seq:
  *    inotify_watch(path, processor)
@@ -18,53 +38,52 @@
  *
  *    Inotify.watch('/tmp', Proc.new { |files| p files })
  */
-static VALUE inotify_watch(VALUE self, VALUE path, VALUE proc)
+static VALUE inotify_watch(VALUE self, VALUE path, VALUE processor)
 {
   int length, i;
   int notify;
   int watch;
   char buffer[BUF_LEN];
 
+  fd_set rfds;
+  struct timeval tv;
+  int retval;
+
   notify = inotify_init();
 
-  if (notify < 0) perror( "inotify_init" );
+  if (notify < 0) perror("inotify_init");
 
   watch = inotify_add_watch(notify, RSTRING_PTR(path), IN_MODIFY | IN_CREATE | IN_DELETE );
 
-  length = read(notify, buffer, BUF_LEN);
+  for (;;)
+  {
+    FD_ZERO(&rfds);
+    FD_SET(notify, &rfds);
 
-  if (length < 0) perror( "read");
+    tv.tv_sec = 0;
+    tv.tv_usec = 500;
 
-  i = 0;
-  while(i < length) {
-    struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
-    if ( event->len ) {
-      if ( event->mask & IN_CREATE ) {
-        if ( event->mask & IN_ISDIR ) {
-          printf( "The directory %s was created.\n", event->name );       
+    retval = select(notify + 1, &rfds, NULL, NULL, &tv);
+
+    if (retval < 0) {
+      perror("select()");
+      return Qfalse;
+    } else if (FD_ISSET(notify, &rfds)) {
+      length = read(notify, buffer, BUF_LEN);
+      if (length < 0) perror("read");
+    
+      i = 0;
+      while(i < length) {
+        struct inotify_event *event = (struct inotify_event *)&buffer[i];
+        if (event->len) {
+          rb_funcall(processor, rb_intern("call"), 2,
+            rb_str_new2(event->name),
+            inotify_event_flags(event->mask)
+          );
         }
-        else {
-          printf( "The file %s was created.\n", event->name );
-        }
-      }
-      else if ( event->mask & IN_DELETE ) {
-        if ( event->mask & IN_ISDIR ) {
-          printf( "The directory %s was deleted.\n", event->name );       
-        }
-        else {
-          printf( "The file %s was deleted.\n", event->name );
-        }
-      }
-      else if ( event->mask & IN_MODIFY ) {
-        if ( event->mask & IN_ISDIR ) {
-          printf( "The directory %s was modified.\n", event->name );
-        }
-        else {
-          printf( "The file %s was modified.\n", event->name );
-        }
+        i += EVENT_SIZE + event->len;
       }
     }
-    i += EVENT_SIZE + event->len;
   }
 
   inotify_rm_watch(notify, watch);
